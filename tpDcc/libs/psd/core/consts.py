de4318336146c17ed8d7fdba22eb9bcf6c56a766
Tomas Poveda <tpovedatd@gmail.com>
2021-03-27 18:09:20 +0100

    Moved modules from tpDcc-libs-python

---
+++ tpDcc/libs/psd/core/consts.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Module that constant definitions used by tpDcc-libs-psd
+"""
+
+from __future__ import print_function, division, absolute_import
+
+LIB_ID = 'tpDcc-libs-psd'
---
+++ tpDcc/libs/psd/core/document.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Module that contains functions to work with Photoshop documents
+"""
+
+from __future__ import print_function, division, absolute_import
+
+
+def find_layers(layer):
+    """
+    Get all layers from a PSD layer
+    """
+
+    layers = list()
+    is_group = False
+    try:
+        layer.layers
+        is_group = True
+    except Exception:
+        pass
+
+    if is_group:
+        for grp_layer in layer.layers:
+            find_layers(grp_layer)
+    else:
+        layers.append(layer)
+    return layers
